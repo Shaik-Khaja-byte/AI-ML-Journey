96b601851df2d5bf2558e7f17b62d0a3533debfd
Shaik-Khaja-Byte <khaja4756@gmail.com>
2025-12-15 15:53:51 +0530

    Deleted and added a new file format

--- Phase 1 - Python fundamentals/Part 1/day1.py
+++
@@ -1,4 +0,0 @@
-# print is a function used to print something on the console.
-# and the words enclosed within "" are called strings
-# \n is a character used to print the info in the next line
-print("Hello \n World!", "How are you?")
